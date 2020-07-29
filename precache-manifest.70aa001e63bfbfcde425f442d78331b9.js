59ddd7e4103805fd72224f0859a9a25bc2dae44a
unknown <rohmansaeful10@gmail.com>
2020-07-29 08:52:42 +0700

    Updates

---
+++ precache-manifest.70aa001e63bfbfcde425f442d78331b9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1695d373998a8304f4e059696b5f75",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cc3756b96b2cd8f62e39",
+    "url": "/my-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "97299b25b48f4d4e1f99",
+    "url": "/my-app/static/css/main.f4f4c5fd.chunk.css"
+  },
+  {
+    "revision": "cc3756b96b2cd8f62e39",
+    "url": "/my-app/static/js/2.f4ce841b.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/my-app/static/js/2.f4ce841b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97299b25b48f4d4e1f99",
+    "url": "/my-app/static/js/main.bcd84b1a.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "bed30ddfa5d434e827c775ac9a3b0d38",
+    "url": "/my-app/static/media/BTSP.bed30ddf.jpg"
+  },
+  {
+    "revision": "535d952c383920c9263f0bdd9cbea534",
+    "url": "/my-app/static/media/CI.535d952c.jpg"
+  },
+  {
+    "revision": "6b19ab42d4444af9d9ede2fd586e37b9",
+    "url": "/my-app/static/media/CSS.6b19ab42.png"
+  },
+  {
+    "revision": "2dab73ac9c38fd25f08c3f151bf4b6b6",
+    "url": "/my-app/static/media/HTML.2dab73ac.png"
+  },
+  {
+    "revision": "013980823b0edc95bbc9b768dcfdb41b",
+    "url": "/my-app/static/media/Movie.01398082.PNG"
+  },
+  {
+    "revision": "0de69f83681ed2dcb919ba9e060524d5",
+    "url": "/my-app/static/media/OFFICE.0de69f83.jpg"
+  },
+  {
+    "revision": "ac35dec77d36f67726fbbe95f7638d96",
+    "url": "/my-app/static/media/Orhil.ac35dec7.PNG"
+  },
+  {
+    "revision": "cd7bb6bec60f6799aa174b4e07b92ea4",
+    "url": "/my-app/static/media/REACT.cd7bb6be.png"
+  },
+  {
+    "revision": "a276cd812d2ac60cac8391cfdc68fea6",
+    "url": "/my-app/static/media/S A B A R_200727.a276cd81.jpg"
+  },
+  {
+    "revision": "eab1a9d8c7192050b77ab7d797c323a1",
+    "url": "/my-app/static/media/Saeful Rohman.eab1a9d8.jpg"
+  },
+  {
+    "revision": "3338afff05f1510f94c47fec823f3eb9",
+    "url": "/my-app/static/media/XD.3338afff.png"
+  },
+  {
+    "revision": "a295928340b97cf2c053227313d7bf57",
+    "url": "/my-app/static/media/fb.a2959283.png"
+  },
+  {
+    "revision": "ccca5d3510f0c6287d582fce8a5d4b7e",
+    "url": "/my-app/static/media/food1.ccca5d35.PNG"
+  },
+  {
+    "revision": "db2a23f682e0e812974f385c8de6f915",
+    "url": "/my-app/static/media/git.db2a23f6.svg"
+  },
+  {
+    "revision": "8c955460d30774293a39ba49f551e6db",
+    "url": "/my-app/static/media/ig.8c955460.png"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/my-app/static/media/javascript.0e4fdce8.png"
+  },
+  {
+    "revision": "6faa55994f0cdc3d4485b74f9b161f35",
+    "url": "/my-app/static/media/momtalk.6faa5599.PNG"
+  },
+  {
+    "revision": "0498f89375e8bb7e33d87210ec34e0bd",
+    "url": "/my-app/static/media/php7.0498f893.jpg"
+  },
+  {
+    "revision": "b48c598364eef626d709336fd3a1cae0",
+    "url": "/my-app/static/media/rent_car.b48c5983.PNG"
+  },
+  {
+    "revision": "e0b1a4a510856a5b709ae3c8471f52d2",
+    "url": "/my-app/static/media/wa.e0b1a4a5.png"
+  }
+]);
